ca100ed7ffb85a5311211c099b8aa2b7528a0a6a
aa <aa@skbkontur.ru>
2017-10-03 16:16:53 +0500

    Add HostEnricher and ServiceNameEnricher

---
+++ Vostok.Logging.Serilog/Enrichers/HostEnricher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Serilog.Core;
+using SerilogEvent = Serilog.Events.LogEvent;
+
+namespace Vostok.Logging.Serilog.Enrichers
+{
+    public class HostEnricher : ILogEventEnricher
+    {
+        private readonly string host;
+
+        public HostEnricher()
+        {
+            try
+            {
+                host = Environment.MachineName;
+            }
+            catch
+            {
+                host = string.Empty;
+            }
+        }
+
+        public void Enrich(SerilogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Host", host));
+        }
+    }
+}
---
+++ Vostok.Logging.Serilog/Enrichers/ServiceNameEnricher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using Serilog.Core;
+using SerilogEvent = Serilog.Events.LogEvent;
+
+namespace Vostok.Logging.Serilog.Enrichers
+{
+    public class ServiceNameEnricher : ILogEventEnricher
+    {
+        private readonly string serviceName;
+
+        public ServiceNameEnricher()
+        {
+            try
+            {
+                serviceName = Process.GetCurrentProcess().ProcessName;
+            }
+            catch
+            {
+                serviceName = string.Empty;
+            }
+        }
+
+        public void Enrich(SerilogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ServiceName", serviceName));
+        }
+    }
+}
