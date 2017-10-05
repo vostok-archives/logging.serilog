4fd2e8660a4e07142d58e9c4ba737276dd61ce00
andrew <Andrew.Kostousov@gmail.com>
2017-10-06 00:35:07 +0500

    + ThreadEnricher

---
+++ Vostok.Logging.Serilog/Enrichers/ThreadEnricher.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using Serilog.Core;
+
+namespace Vostok.Logging.Serilog.Enrichers
+{
+    public class ThreadEnricher : ILogEventEnricher
+    {
+        public void Enrich(global::Serilog.Events.LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Thread", Thread.CurrentThread.Name ?? Thread.CurrentThread.ManagedThreadId.ToString(), false));
+        }
+    }
+}
