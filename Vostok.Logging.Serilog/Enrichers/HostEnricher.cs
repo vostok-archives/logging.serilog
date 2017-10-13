10aff88e655af6a2c774f57c5d35d2b2852c4a5d
spaceorc <spaceorc@skbkontur.ru>
2017-10-13 23:51:08 +0500

    Fix HostEnricher

--- Vostok.Logging.Serilog/Enrichers/HostEnricher.cs
+++ Vostok.Logging.Serilog/Enrichers/HostEnricher.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using Serilog.Core;
+﻿using Serilog.Core;
+using Vostok.Commons;
 using SerilogEvent = Serilog.Events.LogEvent;
 
 namespace Vostok.Logging.Serilog.Enrichers
 {
     public class HostEnricher : ILogEventEnricher
     {
-        private readonly string host;
-
-        public HostEnricher()
-        {
-            try
-            {
-                host = Environment.MachineName;
-            }
-            catch
-            {
-                host = string.Empty;
-            }
-        }
-
         public void Enrich(SerilogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
-            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Host", host));
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Host", HostnameProvider.Get()));
         }
     }
 }
