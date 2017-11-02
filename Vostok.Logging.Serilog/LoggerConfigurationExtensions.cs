c183d1a2fb1a51e2e200580f6894f2983a934bec
spaceorc <spaceorc@skbkontur.ru>
2017-11-02 14:51:23 +0500

    Use static AirlockClient

--- Vostok.Logging.Serilog/LoggerConfigurationExtensions.cs
+++ Vostok.Logging.Serilog/LoggerConfigurationExtensions.cs
@@ -11,6 +11,17 @@ namespace Vostok.Logging.Serilog
     {
         public static LoggerConfiguration Airlock(
             this LoggerSinkConfiguration loggerConfiguration,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
+        {
+            if (loggerConfiguration == null)
+                throw new ArgumentNullException(nameof(loggerConfiguration));
+
+            var sink = new AirlockSink();
+            return loggerConfiguration.Sink(sink, restrictedToMinimumLevel);
+        }
+
+        public static LoggerConfiguration Airlock(
+            this LoggerSinkConfiguration loggerConfiguration,
             IAirlockClient airlockClient,
             string routingKey,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
